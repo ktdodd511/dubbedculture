4ddf5a9b701b73afc8fa52ba7c480c4cade1b29e
katiedodd <ktdodd511@gmail.com>
2019-11-26 14:59:42 -0800

    added audio player

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AudioCard from 'audiocard';
 
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className="container">
         <h1>Coming soon!</h1>
+          <AudioCard
+            art="https://seekjustice.fm/art300.jpg"
+            source="https://dts.podtrac.com/redirect.mp3/seekjustice.fm/media/001.mp3"
+          />
       </div>
     </div>
   );
